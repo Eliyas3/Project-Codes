1133093c7a53b9a8c75eb3abdbb993d01fdfef58
Eliyas3 <eliyaskhanmohammad36@gmail.com>
2026-04-25 13:43:16 +0530

    JS Updated code

--- script.js
+++ script.js
@@ -15,8 +15,9 @@ if(bpm > 100 || spo2 < 95 || temp > 37.5){
 }
 
 else{
-    document.getElementById("status").innerHTML = "Stable";
+    document.geteElementById("status").innerHTML = "Stable";
     document.getElementById("status").style.color = "green";
     document.body.style.background = "#f4f6f9";
 }
+
 },2000);
