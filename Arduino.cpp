f56bf55435064456b732841c19c828b7f13444ce
Eliyas3 <eliyaskhanmohammad36@gmail.com>
2026-04-26 13:39:10 +0530

    Arduino code

---
+++ Arduino.cpp
@@ -0,0 +1,52 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "YOUR_WIFI_NAME";
+const char* password = "YOUR_WIFI_PASSWORD";
+
+String serverName = "http://YOUR_IP_ADDRESS/health_monitor/save.php";
+
+int bpm = 78;
+int spo2 = 98;
+float temp = 36.7;
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting...");
+  }
+
+  Serial.println("WiFi Connected");
+}
+
+void loop() {
+
+  bpm = random(70, 100);
+  spo2 = random(95, 100);
+  temp = random(360, 380) / 10.0;
+
+  if (WiFi.status() == WL_CONNECTED) {
+
+    HTTPClient http;
+
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    String data = "bpm=" + String(bpm) +
+                  "&spo2=" + String(spo2) +
+                  "&temp=" + String(temp);
+
+    int responseCode = http.POST(data);
+
+    Serial.print("Response Code: ");
+    Serial.println(responseCode);
+
+    http.end();
+  }
+
+  delay(5000);
+}
