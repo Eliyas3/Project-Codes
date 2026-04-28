dcc76190b0a3b75478e20ee4fb46317b41b5e1c4
Eliyas3 <eliyaskhanmohammad36@gmail.com>
2026-04-28 23:22:15 +0530

    Dataupload Code

--- Dataupload.cpp
+++ Dataupload.cpp
@@ -5,7 +5,7 @@ HTTPClient http;
 void loop(){
 
 http.begin("http://YOUR_IP/save.php");
-http.addHeader("Content-Type","application/x-www-form-urlencoded");
+http.addeader("Content-Type","application/x-www-form-urlencoded");
 
 String data="bpm=78&spo2=98&temp=36.7";
 
