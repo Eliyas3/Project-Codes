7b4dd6bfaf90a1e2bb821f39adb6e0a60e2218a4
Eliyas3 <eliyaskhanmohammad36@gmail.com>
2026-04-28 23:24:18 +0530

    OLED Display Code

---
+++ oleddisplay.cpp
@@ -0,0 +1,21 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 display(128,64,&Wire,-1);
+
+void setup() {
+display.begin(SSD1306_SWITCHCAPVCC,0x3C);
+display.clearDisplay();
+}
+
+void loop() {
+display.clearDisplay();
+display.setTextSize(1);
+display.setCursor(0,0);
+display.print("BPM: 78");
+display.setCursor(0,20);
+display.print("SpO2: 98%");
+display.display();
+delay(2000);
+}
