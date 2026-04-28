4717b17bd9f2bc72f3d2f376ae8f7f242b52e25e
Eliyas3 <eliyaskhanmohammad36@gmail.com>
2026-04-28 23:25:01 +0530

    OLED Display Code

--- oleddisplay.cpp
+++ oleddisplay.cpp
@@ -15,7 +15,7 @@ display.setTextSize(1);
 display.setCursor(0,0);
 display.print("BPM: 78");
 display.setCursor(0,20);
-display.print("SpO2: 98%");
+display.pint("SpO2: 98%");
 display.display();
 delay(2000);
 }
