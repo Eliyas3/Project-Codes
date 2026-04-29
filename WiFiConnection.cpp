7bd619416e8f874483edabaf2fc753ba4d022c63
Eliyas3 <eliyaskhanmohammad36@gmail.com>
2026-04-29 12:36:19 +0530

    WiFi Connection ode

---
+++ WiFiConnection.cpp
@@ -0,0 +1,19 @@
+#include <WiFi.h>
+
+const char* ssid = "YOUR_WIFI";
+const char* password = "YOUR_PASS";
+
+void setup() {
+Serial.begin(115200);
+WiFi.begin(ssid,password);
+
+while(WiFi.status()!=WL_CONNECTED){
+delay(1000);
+Serial.println("Connecting...");
+}
+
+Serial.println("Connected");
+Serial.println(WiFi.localIP());
+}
+
+void loop(){}
