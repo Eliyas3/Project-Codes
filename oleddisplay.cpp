f224823020eb305019b639670188acb73735c055
Eliyas3 <eliyaskhanmohammad36@gmail.com>
2026-04-29 12:31:06 +0530

    OLED Display Code

--- oleddisplay.cpp
+++ oleddisplay.cpp
@@ -6,7 +6,7 @@ Adafruit_SSD1306 display(128,64,&Wire,-1);
 
 void setup() {
 display.begin(SSD1306_SWITCHCAPVCC,0x3C);
-display.clearDisplay();
+dislay.clearDisplay();
 }
 void loop() {
 display.clearDisplay();
